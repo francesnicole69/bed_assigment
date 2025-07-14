695de49aa5f4e76ec01f41912fea1f9524fc258d
Alexchua <S10259411@connect.np.edu.sg>
2025-07-14 20:42:26 +0800

    push chathelpermodel

--- Model/ChathelperModel.js
+++ Model/ChathelperModel.js
@@ -1,6 +1,6 @@
 import { Sequelize, DataTypes } from 'sequelize'
 import moment from 'moment'
-import sequelize from './sequelize'
+import sequelize from '../backend/src/models/sequelize'
 
 // 聊天助手
 const ChathelperModel = sequelize.define('ChathelperModel', {
